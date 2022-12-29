1f454a1269b86c4802c3dd7817209c9f75167141
Tomek Kolas <223964@edu.p.lodz.pl>
2022-12-29 11:18:10 +0100

    Small improvement

--- CCSPythonProject/webScraping.py
+++ CCSPythonProject/webScraping.py
@@ -2,35 +2,35 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-review_dict = {'Name': [], 'Date': [], 'Review': []}
-page_amount = int(1)
 
-url = 'https://www.metacritic.com/movie/puss-in-boots-the-last-wish/user-reviews'
+review_dict = {'Name': [], 'Date': [], 'Review': []}
+page_amount = 1
+
+url = 'https://www.metacritic.com/movie/avatar-the-way-of-water'
+# url = sys.argv
+url += '/user-reviews'
 user_agent = {'User-agent': 'Chrome/39.0.2171.95'}
 response = requests.get(url, headers=user_agent)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
 for review in soup.find_all('div', class_='pages'):
-    # if 1==2:
-    #     page_amount = type(int(review.find('li', class_='page last_page').find('a').text))
     if review.find('li', class_='page last_page').find('a').text:
         page_amount = int(review.find('li', class_='page last_page').find('a').text)
     else:
         page_amount = 1
 
-for page in range(0,page_amount): #Remember to update the number of pages
+for page in range(0, page_amount):
 
-    url = 'https://www.metacritic.com/movie/puss-in-boots-the-last-wish/user-reviews?page=' + str(page)
+    url2 = url + '?page=' + str(page)
     user_agent = {'User-agent': 'Chrome/39.0.2171.95'}
-    response = requests.get(url, headers=user_agent)
+    response = requests.get(url2, headers=user_agent)
 
     soup = BeautifulSoup(response.text, 'html.parser')
 
     for review in soup.find_all('div', class_='review pad_top1'):
         review_dict['Name'].append(review.find('span', class_='author').find('a').text)
         review_dict['Date'].append(review.find('span', class_='date').text)
-        # review_dict['rating'].append(review.find('span', class_='review_grade').find_all('div')[0].text)
         if review.find('span', class_='blurb blurb_expanded'):
             review_dict['Review'].append(review.find('span', class_='blurb blurb_expanded').text)
         else:
